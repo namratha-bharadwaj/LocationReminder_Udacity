c9f7b558489e70ac89787a70e9184544e02502b6
Namratha Bharadwaj <nbharadwaj@paypal.com>
2022-11-17 12:33:47 -0800

    add additional tests and validations

--- starter/app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
+++ starter/app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
@@ -6,10 +6,12 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.udacity.project4.locationreminders.MainCoroutineRule
 import com.udacity.project4.locationreminders.data.FakeDataSource
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import com.udacity.project4.locationreminders.data.local.RemindersDao
 import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -27,16 +29,18 @@ class SaveReminderViewModelTest {
 
     private lateinit var viewModel: SaveReminderViewModel
     private lateinit var dataSource: FakeDataSource
+    private lateinit var remindersList: MutableList<ReminderDTO>
 
     @Before
     fun setup() {
-        dataSource = FakeDataSource()
+        remindersList = mutableListOf()
+        dataSource = FakeDataSource(remindersList)
         viewModel = SaveReminderViewModel(ApplicationProvider.getApplicationContext(), dataSource)
     }
 
     @Test
     fun givenAReminder_whenSaveReminderIsCalled_savesTheReminderToTheDB() = runBlockingTest {
-        val remindersList = mutableListOf<ReminderDTO>()
+        remindersList.clear()
         val reminder1 = ReminderDataItem(
             title = "Title1",
             description = "Description1",
@@ -48,25 +52,35 @@ class SaveReminderViewModelTest {
 
         assert(remindersList.size == 1)
         assert(remindersList[0].title.equals("Title1"))
+
+        val value = viewModel.showToast.value
+        Assert.assertEquals(value, "Reminder Saved !")
     }
 
     @Test
     fun givenReminderDataIsNull_validateDataReturnsFalse() = runBlockingTest {
         val reminder: ReminderDataItem? = null
-        assert(!viewModel.validateEnteredData(reminder))
+
+        val isEnteredDataValid = viewModel.validateEnteredData(reminder)
+        assert(!isEnteredDataValid)
+        val snackbarIntValue = viewModel.showSnackBarInt.value
+        Assert.assertEquals(2131820601, snackbarIntValue)
     }
 
     @Test
-    fun givenNullLocation_validateDataReturnsFalse() = runBlockingTest {
-        val reminderItem = ReminderDataItem(
-            title = "Title",
-            description = "description",
+    fun givenReminderLocationIsNull_validateDataReturnsFalse() = runBlockingTest {
+        val reminder = ReminderDataItem(
+            title = "Title1",
+            description = "Description1",
             location = null,
             latitude = 0.0,
             longitude = 0.0
         )
 
-        assert(!viewModel.validateEnteredData(reminderItem))
+        val isEnteredDataValid = viewModel.validateEnteredData(reminder)
+        assert(!isEnteredDataValid)
+        val snackbarIntValue = viewModel.showSnackBarInt.value
+        Assert.assertEquals(2131820602, snackbarIntValue)
     }
 
     @Test
